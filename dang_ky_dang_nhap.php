e1559011965d963c06d262d13dbc085b7cc474bc
phamthinhuquynh12 <quynhpham1112002@gmail.com>
2024-02-25 11:24:41 +0700

    push code

---
+++ dang_ky_dang_nhap.php
@@ -0,0 +1,27 @@
+<div class="panel panel-primary" style="width: 280px; margin-left: 5px; border: 0.1px solid #ec522b; margin-top:15px; ">
+    <div class="panel-heading" id="dang_nhap" style="cursor: pointer; border: 0.1px solid #ec522b; background-color: #ec522b; text-align: center; "><span>ĐĂNG NHẬP</span></div>
+    <div id="dang-nhap-form" >
+		<form action="?menu=exec_dang_nhap" method="post">
+			<table>
+				<tr>
+					<td><label for="user">Tên đăng nhập:</label><br><input type="text" class="form-control" name="username" id="user" placeholder="Nhập tên đăng nhập..." required></td>
+				</tr>
+				<tr>
+					<td><label for="pass" style="margin-top: 15px;">Mật khẩu:</label><br><input type="password" class="form-control"  name="password" id="pass" placeholder="Nhập mật khẩu..." required></td>
+				</tr>
+				<tr>
+					<td colspan="2"><input style="width: 100px; margin-left: 80px; margin-top: 15px; border: 0.1px solid #ec522b; background-color: #ec522b;" type="submit" class="btn btn-primary" name="submit" value="Đăng nhập"></td>
+				</tr>
+				<tr>
+    				<td style="padding-top: 10px;">Đăng nhập với quyền quản lý <a href='quan_ly/index.php' style='color: #ec522b;'>Tại đây</a></td>
+				</tr>
+				<tr>
+    				<td style="padding-top: 10px;">Đăng nhập với quyền quản trị <a href='quan_tri/index.php' style='color: #ec522b;'>Tại đây</a></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+	</div>
+<div class="panel panel-primary" style="width: 280px; margin-left: 5px; border: 0.1px solid #ec522b; margin-top:30px;">
+    <div class="panel-heading" id="dang_ky" style="cursor: pointer; border: 0.1px solid #ec522b; background-color: #ec522b; text-align: center;"><span>ĐĂNG KÝ</span></div>
+</div>
